79e3e9b935089b0749a5e5bc18ba1f814b302ca5
PopChicken <jimmy.jino@hotmail.com>
2022-06-10 16:44:43 +0800

    🎈 perf(app): 兼容noreload初始化

--- acss_app/apps.py
+++ acss_app/apps.py
@@ -3,12 +3,17 @@ import os
 from django.apps import AppConfig
 
 
+init_flag = True
+
+
 class acssAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'acss_app'
 
     def ready(self) -> None:
-        if os.environ.get('RUN_MAIN', None) != 'true':
+        global init_flag
+        if init_flag is False:
             return
+        init_flag = False
         from acss_app.service.schd import on_init as on_schd_init
         on_schd_init()
